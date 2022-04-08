876ec520dbb6fe6e90753cebec76855a9e548cf5
Sofonias <sofonias@example.com>
2022-04-08 03:59:28 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
