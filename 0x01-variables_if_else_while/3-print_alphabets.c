5450ae42fb38349e9c87e47e55a0765f67c0b3cc
Sofonias <sofonias@example.com>
2022-04-08 03:42:02 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x, y;
+for (x = 97; x<= 122; x++)
+{
+putchar(x);
+}
+for (y = 65; y <= 90; y++)
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
