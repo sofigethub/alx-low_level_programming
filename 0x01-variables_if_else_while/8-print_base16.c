b00adabe2796abc2c30e1af30a76125c0b6b6919
Sofonias <sofonias@example.com>
2022-04-08 04:08:00 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+for (a = 97; a <= 102; a++)
+{
+putchar(a)
+}
+putchar('\n');
+return (0);
+}
