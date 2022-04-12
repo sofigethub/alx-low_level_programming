09815bffe2673c21fc32c0cbe47242c777acf30a
Sofonias <sofonias@example.com>
2022-04-12 13:52:14 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,14 +3,10 @@
 
 
 /**
+ * main - sums 3-5_cous - Entry point
  *
- *  * main - sums 3-5_cous - Entry point
- *
- *   *
- *
- *    * Return:  (0)
- *
- *     */
+ * Return:  (0)
+ */
 
 
 
@@ -18,70 +14,60 @@ int main(void)
 
 {
 
-		int i = 0;
+	int i = 0;
 
-			int m3 = 3;
+	int m3 = 3;
 
-				int m5 = 5;
+	int m5 = 5;
 
-					int sum = 0;
+	int sum = 0;
 
-						int lim = 1024;
+	int lim = 1024;
 
+	while (i == 0)
 
+	{
 
-							while (i == 0)
+		if (m3 < lim)
 
-									{
+		{
 
+			sum += m3;
 
+			m3 += 3;
 
-												if (m3 < lim)
+		}
 
-															{
+		if (m5 < lim && (m5 % 3) != 0)
 
-																			sum += m3;
+		{
 
-																						m3 += 3;
+			sum += m5;
 
-																								}
+			m5 += 5;
 
+		}
 
+		else if (m5 % 3 == 0)
 
-														if (m5 < lim && (m5 % 3) != 0)
+		{
 
-																	{
+			m5 += 5;
 
-																					sum += m5;
+		}
 
-																								m5 += 5;
+		if (m3 >= lim && m5 >= lim)
 
-																										}
+		{
 
-																else if (m5 % 3 == 0)
+			i = 1;
 
-																			{
+		}
 
-																							m5 += 5;
+	}
 
-																									}
+	printf("%d\n", sum);
 
-
-
-																		if (m3 >= lim && m5 >= lim)
-
-																					{
-
-																									i = 1;
-
-																											}
-
-																			}
-
-								printf("%d\n", sum);
-
-									return (0);
+	return (0);
 
 }
-
-
