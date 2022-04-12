9d037474626ce519ad77d3b4a0b602d595cbfe3b
Sofonias <sofonias@example.com>
2022-04-12 12:15:00 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,6 +9,7 @@ void print_to_98(int n)
 if (n < 98)
 {
 for (n = n; n < 98; n++)
+{
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 _putchar(',');
@@ -16,9 +17,11 @@ _putchar(' ');
 _putchar((98 / 10) + '0');
 _putchar((98 % 10) + '0');
 }
+}
 else
 {
 for (n = n; n > 98; n--)
+{
 _putchar((n / 10) + '0');                                        
 _putchar((n % 10) + '0');
 _putchar(',');                                                   
