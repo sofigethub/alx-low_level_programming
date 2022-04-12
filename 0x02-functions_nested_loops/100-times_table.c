3770cf769777bd6bc3ee5cffb43a2e096ff8bf84
Sofonias <sofonias@example.com>
2022-04-12 10:26:38 -0700

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-int _putchar(char c);
+#include "main.h"
 
 /**
  * print_times_table - print the times table until n
@@ -8,46 +8,33 @@ int _putchar(char c);
  *
  * Return: No return
  */
+
 void print_times_table(int n)
 {
 int i;
 int j;
 if (n >= 0 && n <= 15)
 {
-for (i = 0; i <= n; i++)
+for (i=0, i<=n, i++)
 {
-for (j = 0; j <= n; j++)
+for (j=0, j<=n, j++)
 {
-if ((i * j) < 10)
+k= i*j;
+if (j < n)
 {
-if (j != 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-}
-_putchar((j * i) + '0');
-}
-else if ((i * j) < 100)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar((((j * i) / 10) % 10) + '0');
-_putchar(((j * i) % 10) + '0');
-}
-else if ((i * j) < 1000)
-{
-_putchar(' ');
-_putchar((((j * i) / 100) % 10) + '0');
-_putchar((((j * i) / 10) % 10) + '0');
-_putchar(((j * i) % 10) + '0');
-}
-if (j != (n))
-{
+_putchar(k);
 _putchar(',');
+_putchar (' ');
+}
+else
+{
+_putchar(k);
 }
 }
 _putchar('\n');
 }
+}
+else
+{
 }
 }
