4f4aa29f7038285652d97b6182847bca09e4195a
Sofonias <sofonias@example.com>
2022-04-13 02:15:04 -0700

    3. Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,54 +10,11 @@
  * Return: void
  */
 
-int convert_day(int month, int day)
-{
-switch (month)
-{
-case 2:
-day = 31 + day;
-break;
-case3:
-day = 59 + day;
-break;
-case 4:
-day = 90 + day;
-break;
-case 5:
-day = 120 + day;
-break;
-case 6:
-day = 151 + day;
-break;
-case 7:
-day = 181 + day;
-break;
-case 8:
-day = 212 + day;
-break;
-case 9:
-day = 243 + day;
-break;
-case 10:
-day = 273 + day;
-break;
-case 11:
-day = 304 + day;
-break;
-case 12:
-day = 334 + day;
-break;
-default:
-break;
-}
-return (day);
-}
-
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0) || (year % 400 == 0) && !(year % 100 == 0))
+if (year % 4 == 0 || ((year % 400 == 0) && !(year % 100 == 0)))
 {
-if (month >= 2 && day >= 60)
+if (month > 2 && day >= 60)
 {
 day++;
 }
@@ -67,27 +24,14 @@ printf("Remaining days: %d\n", 366 - day);
 else
 {
 if (month == 2 && day == 60)
-}
+{
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
+printf("Remaining days: %d\n", 365 - day)
+							
 }
 }
-}
-
-int main(void)
-{
-int month;
-int day;
-int year;
-month = 4;
-day = 1;
-year = 1997;
-printf("Date: %02d/%02d/%04d\n", month, day, year);
-day = convert_day(month, day);
-print_remaining_days(month, day, year);
-return (0);
 }
