4642f5f697c4614f9c52dd7090a10897dd18f4e8
Sofonias <sofonias@example.com>
2022-04-18 11:01:13 -0700

    11. Don not hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print password.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+int num;
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
+}
