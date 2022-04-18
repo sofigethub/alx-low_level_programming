a65fd6e910a4dab30ee75b935bc8161092f53b2a
Sofonias <sofonias@example.com>
2022-04-18 11:14:29 -0700

    11. Don not hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,9 +11,26 @@
 
 int main(void)
 {
-int num;
-srand(time(0));
-num = rand();
-printf("%i\n", num);
+int ascii = 2772, i = 0, j, random;
+char password[100];
+time_t t;
+srand((int) time(&t));
+while (ascii > 126)
+{
+random = rand() % 126;
+password[i] = random;
+ascii -= random;
+i++;
+}
+if (ascii > 0)
+password[i] = ascii;
+else
+{
+i--;
+}
+for (j = 0; j <= i; j++)
+{
+printf("%c", password[j]);
+}
 return (0);
 }
