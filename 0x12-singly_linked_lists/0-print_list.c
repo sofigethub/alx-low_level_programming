b7b90429776b191e0eefc1b55533437368141a59
Sofonias <sofonias@example.com>
2022-05-19 06:23:36 -0700

    0x12. C - Singly linked lists Task 0-2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,12 +3,12 @@
 /**
  * print_list - prints all the elements  a list_t list.
  * @h: singly linked list.
+ *
  * Return: number of elements in the list.
  */
 size_t print_list(const list_t *h)
 {
-size_t sofi;
-sofi = 0;
+size_t sofi = 0;
 
 while (h)
 {
